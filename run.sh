82eb8683e442f7cf1b2b3519f9931e17a7163bbf
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2021-07-14 03:50:40 -0400

    Add DRMAA opts

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#SBATCH -p priopark
+#SBATCH -A park_contrib
+#SBATCH -t 120:00:00
+#SBATCH --mem=7G
 
 dryrun=$1
 
@@ -6,7 +10,18 @@ if [ "x$dryrun" != 'x' ]; then
     flags="--dryrun --quiet"
 fi
 
+module load slurm-drmaa
+
+#export TMPDIR=$(realpath try3/tmp)
+#echo "TMPDIR=$TMPDIR"
+
+# tmpdir is necessary for using workflow.source to properly find
+# scripts from within module calls.
 snakemake $flags \
     --dir try3 \
+    --latency-wait 60 \
+    --keep-going \
     -s Snakefile \
-    -C INPUT_MANIFEST=`realpath INPUT_MANIFEST` ROADMAP_EIDS=`realpath ROADMAP_EIDS_WITH_H3K27ac`
+    --max-inventory-time 0 \
+    -j 1000 \
+    --drmaa ' -p park -A park_contrib --mem={resources.mem} -t 12:00:00 -o /n/data1/hms/dbmi/park/jluquette/glia/analysis/try3/cluster-logs/slurm-%A.log'

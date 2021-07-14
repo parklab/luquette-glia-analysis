5040c2a7c6b7ba07dc17ec95a27e25317414b44e
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2021-07-14 03:51:55 -0400

    Add option to skip bigWigAverageOverBed if given a .sig file

--- scripts/make_qbed_from_bigwig.sh
+++ scripts/make_qbed_from_bigwig.sh
@@ -3,9 +3,12 @@
 set -euo pipefail
 
 if [ $# -lt 6 ]; then
-    echo "usage: $0 n_quantiles tiles.bed signal.bigwig out.qbed tmpout tmpout2 [ metadata1 ... metadataN ]"
+    echo "usage: $0 n_quantiles tiles.bed signal.{bigwig|sig} out.qbed tmpout tmpout2 [ metadata1 ... metadataN ]"
+    echo "if the signal file ends in the (case sensitive) .sig extension, then"
+    echo "    bigWigAverageOverBed is not run it is assumed that the .sig file"
+    echo "    is in the correct format."
     echo "metadata should be supplied as TAG=VALUE pairs. Neither TAG nor VALUE"
-    echo "may contain a semi-colon (;)."
+    echo 'may contain a semi-colon (;).'
     exit 1
 fi
 
@@ -18,6 +21,14 @@ tmpout2=$6
 shift 6
 metadata=("$@")
 
+if [ "x$(echo $bigwig | grep -c '\.sig$')" == "x0" ]; then
+    is_sig=FALSE
+    echo "Signal file is type=BIGWIG"
+else
+    is_sig=TRUE
+    echo "Signal file is type=SIGNAL, a file produced by bigWigAverageOverBed"
+fi
+    
 if [ "x$(echo $metadata | grep -c ';')" != "x0" ]; then
     echo "metadata TAG=VALUE pairs must not contain semicolons"
     exit
@@ -50,8 +61,13 @@ fi
 
 # column 5 of tmpout is mean with non-covered bases counted as 0
 # column 6 of tmpout is mean over just covered bases
-echo "Averaging bigwig over tiles.."
-( bigWigAverageOverBed $bigwig $tilesbed $tmpout )
+if [ $is_sig == "FALSE" ]; then
+    echo "Averaging bigwig over tiles.."
+    ( bigWigAverageOverBed $bigwig $tilesbed $tmpout )
+else
+    tmpout=$bigwig
+    echo "Skipping bigWigAverageOverBed because type=SIGNAL, using tmpout=$tmpout"
+fi
 
 
 # Map scores to quantiles and write out the quantiles for every tile,
@@ -62,6 +78,6 @@ echo "Converting to quantiles (n=$nquantiles)"
 Rscript -e 'library(data.table); keep <- fread("'$tilesbed'")[[5]]; score <- fread("'$tmpout'")[[5]]; score[keep == 0] <- NA; q <- findInterval(score, quantile(score, na.rm=T, probs=1:'$nquantiles'/'$nquantiles'), rightmost.closed=TRUE)+1; write(paste(q, score, sep="\t"), ncolumns=1, file="'$tmpout2'")'
 
 # tilesbed has 5 columns, tmpout2 has 2
-echo "Writing qbed.."
+echo "Writing qbed '$outqbed'.."
 (echo "$metatags" ;
  paste $tilesbed $tmpout2 | cut -f1-3,6-7) > $outqbed

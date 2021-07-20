602ec061293bfac98acca0c1667d51d4d9cc2f9d
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2021-07-19 21:48:21 -0400

    Fix typos and use all libraries

--- scatac_manifest_helper.sh
+++ scatac_manifest_helper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # manually encoded library<N> -> sample name map via sed
+echo "datasource,libid,sample,celltype,file"
 for f in ../figures/scatacseq/bigwig/*.bigwig; do
     libstr=$(echo $f|cut -f4 -d\.)
     libid=$(echo $libstr | cut -f1 -d_)
     celltype=$(echo $libstr|cut -f2- -d_)
     echo scatacseq,$libid,$celltype,data/$(echo $f|cut -f3- -d/)
-done | sed -e 's/,library1,/,library1,1278,/' -e's/,library2,/,library2,1465-S3PFC-1_03132020_Analysis,/' -e 's/,library3,/,library3,1465-S3PFC-2_03132020_Analysis,/' -e's/,library4,/,library4,4638,/' -e 's/,library5,/,library5,4643,/' -e's/,library6,/,library6,5087_highdp,/' -e's/,library7,/,library7,5219_highdp,/' -e 's/,libr
+done | sed -e 's/,library1,/,library1,1278,/' -e's/,library2,/,library2,1465-S3PFC-1_03132020_Analysis,/' -e 's/,library3,/,library3,1465-S3PFC-2_03132020_Analysis,/' -e's/,library4,/,library4,4638,/' -e 's/,library5,/,library5,4643,/' -e's/,library6,/,library6,5087_highdp,/' -e's/,library7,/,library7,5219_highdp,/' -e 's/,library8,/,library8,5559,/' -e 's/,library9,/,library9,5817_highdp,/' -e's/,library10,/,library10,5823,/' -e's/,library11,/,library11,5871_highdp,/' -e 's/,librarymerged,/,librarymerged,merged,/'

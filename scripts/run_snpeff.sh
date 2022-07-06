4f433dce683cfed378d193a81bea133326d52305
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2022-07-06 05:41:16 -0400

    Add snpEff

---
+++ scripts/run_snpeff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "usage: $0 input.vcf output.vcf"
+    exit 1
+fi
+
+snpeff=scripts/snpEff.jar
+
+invcf=$1
+outvcf=$2
+
+if [ -f $outvcf ]; then
+    echo "output file $outvcf already exists, please delete it first"
+    exit 1
+fi
+
+
+java -jar $snpeff -t -noStats -v hg19 $invcf > $outvcf

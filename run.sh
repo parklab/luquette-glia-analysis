3fa5f8694e7c6ca8a5ea555317ceb8160f991f67
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-07-07 10:10:39 -0400

    Disable job restarting for now

--- run.sh
+++ run.sh
@@ -22,9 +22,9 @@ snakemake $flags \
     --latency-wait 60 \
     --keep-going \
     -s snakemake/Snakefile \
-    --restart-times 2 \
     --max-inventory-time 0 \
     --max-threads 12 \
     -j 1000 \
     --drmaa ' -p priopark -A park_contrib --mem={resources.mem} -t 24:00:00 -o cluster-logs/slurm-%A.log'
+    #--restart-times 2 \
     #--max-status-checks-per-second 0.1 \

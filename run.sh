fe8aca00160d06f503c6f2a9d504699b9e394a7e
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2022-07-09 10:40:47 -0400

    More common runtime options

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ jobflag='-j 20'
 kgflag=''
 flags=''
 drmaaflag=''
+usedrmaa='false'
 
 if [ "x$word" == 'xdry' ]; then
     flags="--dryrun --quiet" # --reason"
@@ -20,9 +21,9 @@ elif [ "x$word" == 'xlocal' ]; then
     jobflag='-j 20'
     kgflag='--keep-going'
 else
+    usedrmaa='true'
     jobflag='-j 1000'
     kgflag='--keep-going'
-    drmaaflag="--drmaa ' -p priopark -A park_contrib --mem={resources.mem} -c {threads} -t 24:00:00 -o cluster-logs/slurm-%A.log'"
     flags="--max-status-checks-per-second 0.1" # --restart-times 2"
 fi
 
@@ -30,12 +31,25 @@ fi
 
 #export TMPDIR=$(realpath try3/tmp)
 #echo "TMPDIR=$TMPDIR"
-
 # tmpdir is necessary for using workflow.source to properly find
 # scripts from within module calls.
-snakemake $flags \
-    --dir . \
-    --latency-wait 60 $kgflag \
-    -s snakemake/Snakefile \
-    --max-threads 12 $jobflag $drmaaflag \
-    #--max-inventory-time 0 \
+# XXX: doesn't seem to be the case anymore?
+
+
+
+# I can't get $drmaaflags to substitute properly because of the internal 's
+if [ $usedrmaa == "true" ]; then
+    snakemake $flags \
+        --dir . \
+        --latency-wait 60 $kgflag \
+        -s snakemake/Snakefile \
+        --max-threads 12 $jobflag \
+        --drmaa ' -p priopark -A park_contrib --mem={resources.mem} -c {threads} -t 24:00:00 -o cluster-logs/slurm-%A.log'
+else
+    snakemake $flags \
+        --dir . \
+        --latency-wait 60 $kgflag \
+        -s snakemake/Snakefile \
+        --max-threads 12 $jobflag \
+        #--max-inventory-time 0 \
+fi

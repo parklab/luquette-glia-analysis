88cf26ea7fa9602f67cdf71e0c6271549dd0da2c
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2022-07-09 09:45:46 -0400

    Add some common runtime options

--- run.sh
+++ run.sh
@@ -4,10 +4,20 @@
 #SBATCH -t 120:00:00
 #SBATCH --mem=7G
 
-dryrun=$1
+word=$1
 
-if [ "x$dryrun" != 'x' ]; then
+jobflag='-j 20'
+kgflag=''
+flags=''
+drmaaflag="--drmaa \' -p priopark -A park_contrib --mem={resources.mem} -c {threads} -t 24:00:00 -o cluster-logs/slurm-%A.log\'"
+
+if [ "x$word" == 'dry' ]; then
     flags="--dryrun --quiet" # --reason"
+elif [ "x$word" == 'test' ]; then
+    jobflag='-j 1'
+    kgflag=''
+else
+    kgflag='--keep-going'
 fi
 
 #module load slurm-drmaa
@@ -19,12 +29,9 @@ fi
 # scripts from within module calls.
 snakemake $flags \
     --dir . \
-    --latency-wait 60 \
-    --keep-going \
+    --latency-wait 60 $kgflag \
     -s snakemake/Snakefile \
     --max-inventory-time 0 \
-    --max-threads 12 \
-    -j 20 \
-    #-j 1000 --drmaa ' -p priopark -A park_contrib --mem={resources.mem} -c {threads} -t 24:00:00 -o cluster-logs/slurm-%A.log'
+    --max-threads 12 $jobflag $drmaaflag \
     #--restart-times 2 \
     #--max-status-checks-per-second 0.1 \

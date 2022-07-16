b6e079077229b9604303d6602ed21000d6a65899
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-07-15 21:02:57 -0400

    Integrate into snakemake

--- snakemake/scripts/fig4_panel_a.R
+++ snakemake/scripts/fig4_panel_a.R
@@ -1,12 +1,56 @@
 #!/usr/bin/env Rscript
 
-library(GenomicRanges)
-library(mutenrich)
-library(data.table)
-library(svglite)
+# detect script being run by snakemake
+# if so, make a mock commandArgs function
+if ('snakemake' %in% ls()) {
+    logfile <- snakemake@log[[1]]
+    con <- file(logfile, 'w')
+    sink(con, type='output')
+    sink(con, type='message')
 
-nmut <- get(load('/n/data1/hms/dbmi/park/jluquette/glia/analysis/try3/input/neuron___mut___A.rda'))
-omut <- get(load('/n/data1/hms/dbmi/park/jluquette/glia/analysis/try3/input/oligo___mut___A.rda'))
+    commandArgs <- function(...) unlist(c(
+        snakemake@input['neuron_muts'],
+        snakemake@input['oligo_muts'],
+        snakemake@input['tiles'],
+        snakemake@output['pdf'],
+        snakemake@output['svg'],
+        snakemake@output['csv'],
+        snakemake@input['cancer_bigwigs']
+    ))
+    cat('Got command line arguments from snakemake:\n')
+    print(commandArgs())
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) < 7) {
+    stop('usage: fig3_panel_a.R neuron_muts.csv oligo_muts.csv genome_tiles.bed out.pdf out.svg out.csv cancer1.bigwig [ cancer2.bigwig ... cancerN.bigwig ]')
+}
+
+
+neuron.muts <- args[1]
+oligo.muts <- args[2]
+tile.file <- args[3]
+out.pdf <- args[4]
+out.svg <- args[5]
+out.csv <- args[6]
+cancer.fs <- args[-(1:6)]
+
+for (f in c(out.pdf, out.svg, out.csv)) {
+    if (file.exists(f))
+        stop(paste('output file', f, 'already exists, please delete it first'))
+}
+
+suppressMessages(library(GenomicRanges))
+suppressMessages(library(data.table))
+suppressMessages(library(mutenrich))
+suppressMessages(library(extrafont))
+suppressMessages(library(svglite))
+if (!("Arial" %in% fonts()))
+    stop("Arial font not detected; did you load extrafonts and run font_import() with the appropriate path?")
+
+
+nmut <- get(load(neuron.muts))
+omut <- get(load(oligo.muts))
 
 
 count.muts <- function(tiles, x)
@@ -16,32 +60,24 @@ gr2 <- function (bed, seqinfo = NULL, add.chr.prefix = FALSE) {
     ret <- GenomicRanges::GRanges(seqnames = bed[[1]],
         ranges = IRanges::IRanges(start = bed[[2]], bed[[3]]))
     ret$keep <- bed[,5] != 0
+    ret$mean.dp <- bed[,6]
     ret
 }
 
-tiles <- gr2(fread('/n/data1/hms/dbmi/park/jluquette/glia/analysis/try3/alignability/genome_tiles/genome_tiles_1000000binsize.bed'))
+tiles <- gr2(fread(tile.file))
 
 nct <- count.muts(tiles, nmut)
 oct <- count.muts(tiles, omut)
 
-cancer.fs <- list.files(path='/n/data1/hms/dbmi/park/jluquette/glia/analysis/try3/enrichment/cancer_snvdens/quantile/qbed',
-    pattern='cancer_snvdens___.*___normdens.1000000binsize_10quantiles.qbed',
-    full.names=T)
+#cancer.fs <- list.files(path='/n/data1/hms/dbmi/park/jluquette/glia/analysis/try3/enrichment/cancer_snvdens/quantile/qbed',
+    #pattern='cancer_snvdens___.*___normdens.1000000binsize_10quantiles.qbed',
+    #full.names=T)
 
 cancer.mat <- sapply(cancer.fs, function(f) fread(f,skip=1)[[5]])
 colnames(cancer.mat) <- sapply(strsplit(colnames(cancer.mat), '___'), function(x) x[[2]])
 
 
-# Get mean depth profile across single cells
-depth.fs <- list.files(path='/n/data1/hms/dbmi/park/jluquette/glia/analysis/try3/enrichment/depth/quantile/qbed/',
-    pattern='depth___.*___.*.1000000binsize_50quantiles.qbed', full.names=T)
-
-# instead of sensitivity, try technical covariates as proxies
-depth <- do.call(data.frame, lapply(depth.fs, function(f) fread(f, skip=1)[[5]]))
-colnames(depth) <- sub('.1000000binsize_50quantiles.qbed', '',
-    sapply(strsplit(depth.fs, split='___', fixed=TRUE), function(x) x[3]))
-
-mean.dp <- rowMeans(depth)
+mean.dp <- tiles$mean.dp
 
 dp.q10 <- quantile(mean.dp[tiles$keep], prob=1/10)
 dp.q90 <- quantile(mean.dp[tiles$keep], prob=9/10)
@@ -59,18 +95,20 @@ colors[c('CNS-GBM', 'CNS-Medullo', 'CNS-Oligo', 'CNS-PiloAstro')] <-
     c('orange','black','red','purple')
 
 
-layout(1:2)
-par(mar=c(8,4,3,1))
-barplot(ocor, col=colors[names(ocor)], border=F, las=3, ylim=c(-0.03,0.3),
-    cex.names=0.8,
-    main='Oligo passA SNVs\nno bad bins, 10% <= DP <= 90%, 1 MB bins')
-barplot(ncor, col=colors[names(ncor)], border=F, las=3, ylim=c(-0.03,0.3),
-    cex.names=0.8,
-    main='Neuron passA SNVs\nno bad bins, 10% <= DP <= 90%, 1 MB bins')
-
-# NB. I made the size of these plots manually
-#dev.print(dev=pdf, file='cancer_correlation_normdens_1000000binsize_tileskeep_dp_gt_10pct_and_lt_90pct.pdf')
-#dev.print(dev=svglite, file='cancer_correlation_normdens_1000000binsize_tileskeep_dp_gt_10pct_and_lt_90pct.svg')
+devs=list(pdf, svglite)
+outs=c(out.pdf, out.svg)
+for (i in 1:2) {
+    devs[[i]](width=15, height=5, pointsize=5, file=outs[i])
+    layout(1:2)
+    par(mar=c(8,4,3,1))
+    barplot(ocor, col=colors[names(ocor)], border=F, las=3, ylim=c(-0.03,0.3),
+        cex.names=0.8,
+        main='Oligo passA SNVs\nno bad bins, 10% <= DP <= 90%, 1 MB bins')
+    barplot(ncor, col=colors[names(ncor)], border=F, las=3, ylim=c(-0.03,0.3),
+        cex.names=0.8,
+        main='Neuron passA SNVs\nno bad bins, 10% <= DP <= 90%, 1 MB bins')
+    dev.off()
+}
 
 
 # Get p-values for cor=0 t-tests
@@ -86,3 +124,7 @@ npv <- apply(cancer.mat, 2, function(col) {
         normal=nct[tiles$keep & mean.dp >= dp.q10 & mean.dp <= dp.q90])))
     c(m$r.squared, coef(m)['normal',4])
 })
+
+if ('snakemake' %in% ls()) {
+    sink()
+}

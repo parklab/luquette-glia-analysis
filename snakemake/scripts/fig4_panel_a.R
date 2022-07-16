8df14e5a16b2a0ad438a98799ae9bd0086d4d564
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-07-15 22:01:33 -0400

    Output plotted values to table

--- snakemake/scripts/fig4_panel_a.R
+++ snakemake/scripts/fig4_panel_a.R
@@ -48,10 +48,8 @@ suppressMessages(library(svglite))
 if (!("Arial" %in% fonts()))
     stop("Arial font not detected; did you load extrafonts and run font_import() with the appropriate path?")
 
-
 nmut <- get(load(neuron.muts))
 omut <- get(load(oligo.muts))
-
 
 count.muts <- function(tiles, x)
     countOverlaps(tiles, gr(x, add.chr.prefix=TRUE))
@@ -69,10 +67,6 @@ tiles <- gr2(fread(tile.file))
 nct <- count.muts(tiles, nmut)
 oct <- count.muts(tiles, omut)
 
-#cancer.fs <- list.files(path='/n/data1/hms/dbmi/park/jluquette/glia/analysis/try3/enrichment/cancer_snvdens/quantile/qbed',
-    #pattern='cancer_snvdens___.*___normdens.1000000binsize_10quantiles.qbed',
-    #full.names=T)
-
 cancer.mat <- sapply(cancer.fs, function(f) fread(f,skip=1)[[5]])
 colnames(cancer.mat) <- sapply(strsplit(colnames(cancer.mat), '___'), function(x) x[[2]])
 
@@ -81,34 +75,6 @@ mean.dp <- tiles$mean.dp
 
 dp.q10 <- quantile(mean.dp[tiles$keep], prob=1/10)
 dp.q90 <- quantile(mean.dp[tiles$keep], prob=9/10)
-
-ocor <- sort(apply(cancer.mat, 2, function(col)
-    cor(col[tiles$keep & mean.dp >= dp.q10 & mean.dp <= dp.q90],
-        oct[tiles$keep & mean.dp >= dp.q10 & mean.dp <= dp.q90])))
-ncor <- sort(apply(cancer.mat, 2, function(col)
-    cor(col[tiles$keep & mean.dp >= dp.q10 & mean.dp <= dp.q90],
-        nct[tiles$keep & mean.dp >= dp.q10 & mean.dp <= dp.q90])))
-
-
-colors <- setNames(rep('grey',37), colnames(cancer.mat))
-colors[c('CNS-GBM', 'CNS-Medullo', 'CNS-Oligo', 'CNS-PiloAstro')] <-
-    c('orange','black','red','purple')
-
-
-devs=list(pdf, svglite)
-outs=c(out.pdf, out.svg)
-for (i in 1:2) {
-    devs[[i]](width=6.5, height=2, pointsize=5, file=outs[i])
-    layout(t(1:2))
-    par(mar=c(8,4,3,1))
-    barplot(ocor, col=colors[names(ocor)], border=F, las=3, ylim=c(-0.03,0.3),
-        cex.names=0.8,
-        main='Oligo passA SNVs\nno bad bins, 10% <= DP <= 90%, 1 MB bins')
-    barplot(ncor, col=colors[names(ncor)], border=F, las=3, ylim=c(-0.03,0.3),
-        cex.names=0.8,
-        main='Neuron passA SNVs\nno bad bins, 10% <= DP <= 90%, 1 MB bins')
-    dev.off()
-}
 
 
 # Get correlation, R^2, p-values for cor=0 t-tests
@@ -121,6 +87,7 @@ opv <- do.call(rbind, lapply(1:ncol(cancer.mat), function(colidx) {
     data.frame(Muts="Oligo",Cancer=colnames(cancer.mat)[colidx],
         Correlation=cor(df$cancer, df$normal), R.squared=m$r.squared, P.value=coef(m)['normal',4])
 }))
+opv <- opv[order(opv$Correlation, decreasing=TRUE),]
 npv <- do.call(rbind, lapply(1:ncol(cancer.mat), function(colidx) {
     col <- cancer.mat[,colidx]
     df <- data.frame(
@@ -130,8 +97,28 @@ npv <- do.call(rbind, lapply(1:ncol(cancer.mat), function(colidx) {
     data.frame(Muts="Neuron",Cancer=colnames(cancer.mat)[colidx],
         Correlation=cor(df$cancer, df$normal), R.squared=m$r.squared, P.value=coef(m)['normal',4])
 }))
+npv <- npv[order(npv$Correlation, decreasing=TRUE),]
 
-print(rbind(opv[order(opv$Correlation),], npv[order(npv$Correlation),]))
+fwrite(rbind(opv, npv), file=out.csv)
+
+colors <- setNames(rep('grey',37), colnames(cancer.mat))
+colors[c('CNS-GBM', 'CNS-Medullo', 'CNS-Oligo', 'CNS-PiloAstro')] <-
+    c('orange','black','red','purple')
+
+devs=list(pdf, svglite)
+outs=c(out.pdf, out.svg)
+for (i in 1:2) {
+    devs[[i]](width=6.5, height=2, pointsize=5, file=outs[i])
+    layout(t(1:2))
+    par(mar=c(8,4,3,1))
+    barplot(opv$Correlation, col=colors[opv$Cancer], border=F, las=3, ylim=c(-0.03,0.3),
+        cex.names=0.8,
+        main='Oligo passA SNVs\nno bad bins, 10% <= DP <= 90%, 1 MB bins')
+    barplot(npv$Correlation, col=colors[npv$Cancer], border=F, las=3, ylim=c(-0.03,0.3),
+        cex.names=0.8,
+        main='Neuron passA SNVs\nno bad bins, 10% <= DP <= 90%, 1 MB bins')
+    dev.off()
+}
 
 if ('snakemake' %in% ls()) {
     sink()

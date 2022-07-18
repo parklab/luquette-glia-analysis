72465f1dcd1dc80d83679020ac7229d11880d063
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-07-18 01:48:04 -0400

    Initial commit

--- snakemake/scripts/parse_pcawg_snpeff.sh
+++ snakemake/scripts/parse_pcawg_snpeff.sh
@@ -8,14 +8,14 @@ fi
 invcf=$1
 
 cat $invcf \
-    | grep -v '#'
-    | grep ANN
-    | cut -f 8
-    | tr ';' '\t'
-    | sed 's#TType=##g'
-    | sed 's#Origin=##g'
-    | sed 's#Sample=##g'
-    | sed 's#ANN=##g'
+    | grep -v '#' \
+    | grep ANN \
+    | cut -f 8 \
+    | tr ';' '\t' \
+    | sed 's#TType=##g' \
+    | sed 's#Origin=##g' \
+    | sed 's#Sample=##g' \
+    | sed 's#ANN=##g' \
     | cut -f 1,4 \
     | awk 'BEGIN { OFS="\t"; } { n = split($2, anns, ","); for (i = 1; i <= n; ++i) { print NR, $1, anns[i];  } }' \
     | tr '|' '\t' \

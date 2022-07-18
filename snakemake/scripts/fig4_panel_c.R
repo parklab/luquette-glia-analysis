dc8f61fa4701cf7e362305c9568ed165eac24c80
Joe Luquette <jl@Joes-MacBook-Pro-3.local>
2022-07-17 21:10:19 -0400

    Integrate fig4c scripts into automatic pipeline

--- snakemake/scripts/fig4_panel_c.R
+++ snakemake/scripts/fig4_panel_c.R
@@ -1,17 +1,56 @@
+#!/usr/bin/env Rscript
+
+# detect script being run by snakemake
+# if so, make a mock commandArgs function
+if ('snakemake' %in% ls()) {
+    logfile <- snakemake@log[[1]]
+    con <- file(logfile, 'w')
+    sink(con, type='output')
+    sink(con, type='message')
+
+    commandArgs <- function(...) unlist(c(
+        snakemake@input['neuron_rna'],
+        snakemake@input['neuron_atac'],
+        snakemake@input['oligo_rna'],
+        snakemake@input['oligo_atac'],
+        snakemake@output['pdf'],
+        snakemake@output['svg'],
+        snakemake@output['csv']
+    ))
+    cat('Got command line arguments from snakemake:\n')
+    print(commandArgs())
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) != 7) {
+    stop('usage: fig4_panel_c.R neuron_rna.rda neuron_atac.rda oligo_rna.rda oligo_atac.rda out.pdf out.svg out.csv')
+}
+
+neuron.rna <- args[1]
+neuron.atac <- args[2]
+oligo.rna <- args[3]
+oligo.atac <- args[4]
+out.pdf <- args[5]
+out.svg <- args[6]
+out.csv <- args[7]
+
+for (f in c(out.pdf, out.svg, out.csv)) {
+    if (file.exists(f))
+        stop(paste('output file', f, 'already exists, please delete it first'))
+}
+
+suppressMessages(library(data.table))
 suppressMessages(library(mutenrich))
 suppressMessages(library(extrafont))
 suppressMessages(library(svglite))
-
-
 if (!("Arial" %in% fonts()))
     stop("Arial font not detected; did you load extrafonts and run font_import() with the appropriate path?")
 
-
-# 1MB plots
-#load('scatacseq_foldchange_oligo_AB_1000000.SUMMARY.rda'); aoes.1m <- lapply(es, function(e) e[as.character(1:10),])
-#load('scatacseq_foldchange_neuron_AB_1000000.SUMMARY.rda'); anes.1m <- lapply(es, function(e) e[as.character(1:10),])
-load('scatacseq_foldchange_oligo_AB_1000.SUMMARY.rda'); aoes.1m <- lapply(es, function(e) e[as.character(1:10),])
-load('scatacseq_foldchange_neuron_AB_1000.SUMMARY.rda'); anes.1m <- lapply(es, function(e) e[as.character(1:10),])
+# ATACseq
+load(oligo.atac)
+aoes <- lapply(es, function(e) e[as.character(1:10),])
+load(neuron.atac)
+anes <- lapply(es, function(e) e[as.character(1:10),])
 atacmeta <- emeta
 atacmap <- setNames(c('OPCs', 'Astrocytes', 'Excitatory-Neurons', 'Inhibitory-Neurons',
     'Microglia', 'Oligodendrocytes'),
@@ -22,16 +61,16 @@ print(atacmeta$celltype)
 atacmeta$celltype <- atacmap[levels(atacmeta$celltype)]
 print(atacmeta$celltype)
 
-# 1 MB bins
-#load('scrnaseq_foldchange_oligo_AB_1000000.SUMMARY.rda'); roes.1m <- lapply(es, function(e) e[as.character(1:10),])
-#load('scrnaseq_foldchange_neuron_AB_1000000.SUMMARY.rda'); rnes.1m <- lapply(es, function(e) e[as.character(1:10),])
-load('scrnaseq_foldchange_oligo_AB_1000.SUMMARY.rda'); roes.1m <- lapply(es, function(e) e[as.character(1:10),])
-load('scrnaseq_foldchange_neuron_AB_1000.SUMMARY.rda'); rnes.1m <- lapply(es, function(e) e[as.character(1:10),])
+# RNAseq
+load(oligo.rna)
+roes <- lapply(es, function(e) e[as.character(1:10),])
+load(neuron.rna)
+rnes <- lapply(es, function(e) e[as.character(1:10),])
 rnameta <- emeta
 rnameta$celltype <- levels(rnameta$celltype)
 
 
-# Matches the UMAP figure colors
+# Match the UMAP figure colors
 colors <- setNames(c('#f5c710', '#61d04f', 'black', '#28e2e5', '#cd0bbc', '#FF0000', '#9e9e9e', 'black'),
     c('Astrocytes', 'Endothelial', 'Excitatory-Neurons', 'Inhibitory-Neurons',
       'Microglia', 'Oligodendrocytes', 'OPCs', 'Neurons'))
@@ -44,51 +83,55 @@ textpane <- function(txt, ...) {
 }
 
 
-x11(width=9.5, height=8)
-layout(matrix(1:6, nrow=2,byrow=T), width=c(2,2,1))
-n=5
-yl=c(0.5,1.5)
+devs=list(pdf, svglite)
+outs=c(out.pdf, out.svg)
+for (i in 1:2) {
+    devs[[i]](width=6, height=2.5, pointsize=5, file=outs[i])
 
-# For snRNAseq, exc. and inh. neurons were merged. Can break this
-# out later.
-for (i in 1:n)
-    plot.enrich(es=rnes.1m[[i]], main='Neuron SNVs passAB 1MB bins', type='l', ltype='l',
-        xlab='snRNAseq foldchange (decile)', las=2, 
-        lcol=colors[rnameta$celltype[i]], add=i>1, ylim=yl, bootstrap.ci=F)
-abline(h=1, lty='dashed', col='grey')
-for (i in 1:n)
-    plot.enrich(es=roes.1m[[i]], main='Oligodendrocyte SNVs passAB 1MB bins', type='l', ltype='l',
-        xlab='snRNAseq foldchange (decile)', las=2,
-        lcol=colors[rnameta$celltype[i]], add=i>1, ylim=yl, bootstrap.ci=F)
-abline(h=1, lty='dashed', col='grey')
+    # format:
+    # row1: scRNAseq: neuron plot, oligo plot, legend 
+    # row2: scATACseq: neuron plot, oligo plot, legend 
+    layout(matrix(1:6, nrow=2,byrow=T), width=c(2,2,1))
+    par(mar=c(4,4,2,1))
 
-textpane(x='center', txt=rnameta$celltype, lwd=2, col=colors[rnameta$celltype])
+    n=5
+    yl=c(0.5,1.5)
 
-#dev.print(dev=pdf, file='scrnaseq_foldchange_plots_1m_passAB.pdf')
-#dev.print(dev=svglite, file='scrnaseq_foldchange_plots_1m_passAB.svg')
+    # For snRNAseq, exc. and inh. neurons were merged. Can break this
+    # out in supplement if we want.
+    for (i in 1:n)
+        plot.enrich(es=rnes[[i]], main='Neuron SNVs', type='l', ltype='l',
+            xlab='snRNAseq foldchange (decile)', las=2, 
+            lcol=colors[rnameta$celltype[i]], add=i>1, ylim=yl, bootstrap.ci=F)
+    abline(h=1, lty='dashed', col='grey')
+    for (i in 1:n)
+        plot.enrich(es=roes[[i]], main='Oligodendrocyte SNVs', type='l', ltype='l',
+            xlab='snRNAseq foldchange (decile)', las=2,
+            lcol=colors[rnameta$celltype[i]], add=i>1, ylim=yl, bootstrap.ci=F)
+    abline(h=1, lty='dashed', col='grey')
 
-#x11(width=9.5, height=4)
-#layout(t(1:3), width=c(2,2,1))
-n=6
-# for 1 MB plots
-#yl=c(0.5,1.5)
-yl=c(0.85,1.2)  # for 1 KB plots
+    textpane(x='center', txt=rnameta$celltype, lwd=2, col=colors[rnameta$celltype])
 
-# For snRNAseq, exc. and inh. neurons were merged. Can break this
-# out later.
-for (i in 1:n)
-    plot.enrich(es=anes.1m[[i]], main='Neuron SNVs passAB 1MB bins', type='l', ltype='l',
-        xlab='scATACseq foldchange (decile)', las=2, 
-        lcol=colors[atacmeta$celltype[i]], add=i>1, ylim=yl, bootstrap.ci=F)
-abline(h=1, lty='dashed', col='grey')
-for (i in 1:n)
-    plot.enrich(es=aoes.1m[[i]], main='Oligodendrocyte SNVs passAB 1MB bins', type='l', ltype='l',
-        xlab='scATACseq foldchange (decile)', las=2,
-        lcol=colors[atacmeta$celltype[i]], add=i>1, ylim=yl, bootstrap.ci=F)
-abline(h=1, lty='dashed', col='grey')
+    n=6
+    yl=c(0.5,1.5) # for 1 MB plots
+    #yl=c(0.85,1.2)  # for 1 KB plots
 
-textpane(x='center', txt=atacmeta$celltype, lwd=2, col=colors[atacmeta$celltype])
+    for (i in 1:n)
+        plot.enrich(es=anes[[i]], main='Neuron SNVs', type='l', ltype='l',
+            xlab='scATACseq foldchange (decile)', las=2, 
+            lcol=colors[atacmeta$celltype[i]], add=i>1, ylim=yl, bootstrap.ci=F)
+    abline(h=1, lty='dashed', col='grey')
+    for (i in 1:n)
+        plot.enrich(es=aoes[[i]], main='Oligodendrocyte SNVs', type='l', ltype='l',
+            xlab='scATACseq foldchange (decile)', las=2,
+            lcol=colors[atacmeta$celltype[i]], add=i>1, ylim=yl, bootstrap.ci=F)
+    abline(h=1, lty='dashed', col='grey')
 
-#dev.print(dev=pdf, file='scatacseq_foldchange_plots_1m_passAB.pdf')
-#dev.print(dev=svglite, file='scatacseq_foldchange_plots_1m_passAB.svg')
+    textpane(x='center', txt=atacmeta$celltype, lwd=2, col=colors[atacmeta$celltype])
 
+    dev.off()
+}
+
+if ('snakemake' %in% ls()) {
+    sink()
+}

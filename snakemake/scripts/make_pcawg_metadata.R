ddb328ce0b8a25bd2230a3caa5bf562ad811c1f8
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2022-07-18 00:11:38 -0400

    Add parsers for PCAWG metadata for gene enrichment analysis

---
+++ snakemake/scripts/make_pcawg_metadata.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) != 4)
+    stop('usage: make_metadata_helper.R pcawg_sample_sheet.tsv icgc_donor_ids.txt tcga_donor_ids.txt out.txt')
+
+ss <- args[1]
+icgc.file <- args[2]
+tcga.file <- args[3]
+out.csv <- args[4]
+
+if (file.exists(out.csv))
+    stop(paste('output file', out.csv, 'already exists, please delete it first'))
+
+library(data.table)
+
+x <- fread(ss)
+
+icgc <- fread(icgc.file)
+tcga <- fread(tcga.file)
+
+x[icgc_donor_id %in% icgc[[2]], project := 'icgc']
+x[icgc_donor_id %in% tcga[[2]], project := 'tcga']
+
+# Only select tumors with WGS data, not excluded and only retain one record per donor ID
+# (some donors have multiple tumor samples, e.g.).
+x <- x[library_strategy=='WGS' & grepl('tumour', dcc_specimen_type) & donor_wgs_exclusion_white_gray == 'Whitelist',.SD[1,],by='icgc_donor_id']
+
+y <- rbind(icgc, tcga)
+x[y, tumor := Project_Code, on=c('icgc_donor_id'='Donor_ID')]
+
+x <- x[, .(icgc_donor_id, tumor, tumor_code=sapply(strsplit(dcc_project_code, '-'), head, 1), project, file=paste0('data/pcawg/vcfs/', icgc_donor_id, '.vcf'))]
+
+fwrite(x, file=out.csv)

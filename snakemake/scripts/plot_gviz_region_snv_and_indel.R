61cf529ae04591be1e0681de4e17c9f8c403413c
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2022-09-20 14:44:07 -0400

    Fork script to not require both SNVs and indels

--- snakemake/scripts/plot_gviz_region_snv_and_indel.R
+++ snakemake/scripts/plot_gviz_region_snv_and_indel.R
@@ -1,13 +1,56 @@
-library(Gviz)
-library(scan2)
-library(mutenrich)
+#!/usr/bin/env Rscript
 
-extend=1e6
+# detect script being run by snakemake
+# if so, make a mock commandArgs function
+if ('snakemake' %in% ls()) {
+    logfile <- snakemake@log[[1]]
+    con <- file(logfile, 'w')
+    sink(con, type='output')
+    sink(con, type='message')
+
+    commandArgs <- function(...) unlist(c(
+        snakemake@input['neuron'],
+        snakemake@input['oligo'],
+        snakemake@output['csv'],
+        snakemake@output['pdf'],
+        snakemake@output['svg']
+    ))
+    cat('Got command line arguments from snakemake:\n')
+    print(commandArgs())
+}
+
+args <- commandArgs(trailingOnly=TRUE)
+if (length(args) != 7) {
+    stop('usage: plot_gviz_region.R neuron_snv_profile.csv neuron_indel_profile.csv oligo_snv_profile.csv oligo_indel_profile.csv extend_basepairs out_regions.csv out_prefix_for_plots')
+}
+
+
+neuron_snv.csv <- args[1]
+neuron_indel.csv <- args[2]
+oligo_snv.csv <- args[3]
+oligo_indel.csv <- args[4]
+extend <- as.integer(args[5])
+out.csv <- args[6]
+out.prefix <- args[7]
+
+for (f in c(out.csv)) {
+    if (file.exists(f))
+        stop(paste('output file', f, 'already exists, please delete it first'))
+}
+
+suppressMessages(library(Gviz))
+suppressMessages(library(scan2))
+suppressMessages(library(mutenrich))
+suppressMessages(library(extrafont))
+suppressMessages(library(svglite))
+if (!("Arial" %in% fonts()))
+    stop("Arial font not detected; did you load extrafonts and run font_import() with the appropriate path?")
+
 
 get.annots <- function(chrom, from, to) {
     gtrack <- GenomeAxisTrack()
     itrack <- IdeogramTrack(genome='hg19', chromosome=chrom)
-    refseq <- UcscTrack(genome='hg19', chromosome=chrom, # from=from, to=to,
+    refseq <- UcscTrack(genome='hg19', chromosome=chrom, from=from, to=to,
         table='ncbiRefSeq', track='NCBI RefSeq', trackType='GeneRegionTrack',
         rstarts='exonStarts', rends='exonEnds', gene='name2', symbol='name2',
         transcript='name', strand='strand', fill='#8282d2', name='RefSeq')
@@ -15,16 +58,16 @@ get.annots <- function(chrom, from, to) {
     list(itrack,gtrack,refseq)
 }
 
-ns <- fread('neuron.profile.csv')
+ns <- fread(neuron_snv.csv)
 ns$chr <- paste0('chr', ns$chr)
-os <- fread('oligo.profile.csv')
+os <- fread(oligo_snv.csv)
 os$chr <- paste0('chr', os$chr)
 s <- ns[,.(chr,start,end,muts.smoothed.peryear)]
 s$oligo.muts.smoothed.peryear <- os$muts.smoothed.peryear
 
-ni <- fread('neuron_indel_passAB.profile.csv')
+ni <- fread(neuron_indel.csv)
 ni$chr <- paste0('chr', ni$chr)
-oi <- fread('oligo_indel_passAB.profile.csv')
+oi <- fread(oligo_indel.csv)
 oi$chr <- paste0('chr', oi$chr)
 i <- ni[,.(chr,start,end,muts.smoothed.peryear)]
 i$oligo.muts.smoothed.peryear <- oi$muts.smoothed.peryear
@@ -34,6 +77,8 @@ dstrack <- DataTrack(GRanges(s), name='SNVs per Mb*year', col=1:2, groups=c('neu
 ditrack <- DataTrack(GRanges(i), name='Indels per Mb*year', col=1:2, groups=c('neuron','oligo'), type='l')
 
 # Create full set of sites with Poisson p-value < 0.1 after BH adjustment
+fwrite(rbind(ns,os,ni,oi)[pois.pval.adj < 0.1], file=out.csv)
+
 all.sites <- reduce(GRanges(rbind(ns,os,ni,oi)[pois.pval.adj < 0.1]))
 
 for (i in 1:length(all.sites)) {
@@ -45,16 +90,25 @@ for (i in 1:length(all.sites)) {
     cat('chrom', chrom, '\n')
     cat('from', from, '\n')
     cat('to', to, '\n')
-    as <- get.annots(chrom=chrom)
+    as <- get.annots(chrom=chrom, from=from-(2*extend), to=to+extend*2)
     cat('plotting..\n')
+    
+    devs=list(pdf, svglite)
+    outs=paste0(out.prefix, 'region_', i, c('.pdf', '.svg'))
+    for (j in 1:2) {
+        devs[[j]](width=4, height=6, pointsize=5, file=outs[j])
+        #par(mar=c(4,4,3,1))
+        ht <- HighlightTrack(trackList = list(dstrack, ditrack, as[[3]]),
+                        start = from, end=to, chromosome=chrom)
+        plotTracks(c(as[1:2],ht),
+            from=from-extend, to=to+extend,
+            collapseTranscripts='meta',
+            transcriptAnnotation='symbol',
+            legend=FALSE)
+        dev.off()
+    }
+}
 
-    ht <- HighlightTrack(trackList = list(dstrack, ditrack, as[[3]]),
-                     start = from, end=to, chromosome=chrom)
-    pdf(file=paste0('region_', i, '.pdf'))
-    plotTracks(c(as[1:2],ht),
-        from=from-extend, to=to+extend,
-        collapseTranscripts='meta',
-        transcriptAnnotation='symbol',
-        legend=FALSE)
-    dev.off()
+if ('snakemake' %in% ls()) {
+    sink()
 }

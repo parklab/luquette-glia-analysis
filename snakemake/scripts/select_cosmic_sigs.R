75f1e46d42862e2e2869087146e706aa60ff19a2
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2022-09-21 18:37:12 -0400

    Allow arbitrary number of celltypes

--- snakemake/scripts/select_cosmic_sigs.R
+++ snakemake/scripts/select_cosmic_sigs.R
@@ -9,38 +9,31 @@ if ('snakemake' %in% ls()) {
     sink(con, type='message')
 
     commandArgs <- function(...) unlist(c(
-        snakemake@input[c('neuron','oligo')], snakemake@output['csv']
+        snakemake@output['csv'],
+        snakemake@input
     ))
     cat('Got command line arguments from snakemake:\n')
     print(commandArgs())
 }
 
 args <- commandArgs(trailingOnly=TRUE)
-if (length(args) != 3) {
-    stop("usage: rda_to_csv.r neuron_scores.csv oligo_scores.csv out.csv")
+if (length(args) < 2) {
+    stop("usage: select_cosmic_sigs.R out.csv scored_sigs1.csv [ scored_sigs2.csv ... scored_sigs3.csv ]")
 }
 
-nscore.csv <- args[1]
-gscore.csv <- args[2]
-out.csv <- args[3]
+out.csv <- args[1]
+score.csvs <- args[-1]
 
 if (file.exists(out.csv))
     stop(paste('outut file', out.csv, 'already exists, please delete it first'))
 
 suppressMessages(library(scan2))
 
-n <- fread(nscore.csv)
-g <- fread(gscore.csv)
+scores <- rbindlist(lapply(score.csvs, fread))
 
-n$AgeSignifAdj <- -log10(p.adjust(10^-n$AgeSignif))
-n[, SigIncluded := (AgeSignifAdj >= 2 & PercentContribution >= 1) | PercentStepwiseMeanResidualReduction > 50]
+scores[, SigIncluded := (AgeSignifAdj >= 2 & PercentContribution >= 1) | PercentStepwiseMeanResidualReduction > 50]
 
-g$AgeSignifAdj <- -log10(p.adjust(10^-g$AgeSignif))
-g[, SigIncluded := (AgeSignifAdj >= 2 & PercentContribution >= 1) | PercentStepwiseMeanResidualReduction > 50]
-
-combined <- rbind(n,g)
-
-fwrite(combined, file=out.csv)
+fwrite(scores, file=out.csv)
 
 if ('snakemake' %in% ls()) {
     sink()

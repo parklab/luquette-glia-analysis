e639a30166a2866755dca65c1a2fe505b0dfc9bd
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-01-03 19:14:21 -0500

    Make main figure n=100 genes and add supplementary plot for other n

--- snakemake/scripts/fig4_panel_d.R
+++ snakemake/scripts/fig4_panel_d.R
@@ -12,6 +12,10 @@ if ('snakemake' %in% ls()) {
         snakemake@output['pdf'],
         snakemake@output['svg'],
         snakemake@output['csv'],
+        snakemake@output['supppdf'],
+        snakemake@output['suppsvg'],
+        snakemake@output['fullsupppdf'],
+        snakemake@output['fullsuppsvg'],
         snakemake@input['cancer_ors']
     ))
     cat('Got command line arguments from snakemake:\n')
@@ -19,17 +23,21 @@ if ('snakemake' %in% ls()) {
 }
 
 args <- commandArgs(trailingOnly=TRUE)
-if (length(args) < 4) {
-    stop('usage: fig4_panel_d.R out.pdf out.svg out.csv cancer_odds_ratios1.rda [ cancer_odd_ratios2.rda ... cancer_odds_ratiosN.rda ]')
+if (length(args) < 8) {
+    stop('usage: fig4_panel_d.R out.pdf out.svg out.csv out.supp.pdf out.supp.svg out.fullsupp.pdf out.fullsupp.svg cancer_odds_ratios1.rda [ cancer_odd_ratios2.rda ... cancer_odds_ratiosN.rda ]')
 }
 
 
 out.pdf <- args[1]
 out.svg <- args[2]
 out.csv <- args[3]
-cancer.fs <- args[-(1:3)]
+out.supp.pdf <- args[4]
+out.supp.svg <- args[5]
+out.fullsupp.pdf <- args[6]
+out.fullsupp.svg <- args[7]
+cancer.fs <- args[-(1:7)]
 
-for (f in c(out.pdf, out.svg, out.csv)) {
+for (f in c(out.pdf, out.svg, out.csv, out.supp.pdf, out.supp.svg, out.fullsupp.pdf, out.fullsupp.svg)) {
     if (file.exists(f))
         stop(paste('output file', f, 'already exists, please delete it first'))
 }
@@ -58,11 +66,29 @@ emphasize <- c('CNS-GBM','CNS-Medullo','CNS-Oligo', 'CNS-PiloAstro')
 colors <- setNames(rep('grey',37), colnames(odds.mat))
 colors[emphasize] <- c('orange','black','red','purple')
 
+
 devs=list(pdf, svglite)
 outs=c(out.pdf, out.svg)
 for (i in 1:2) {
-    devs[[i]](width=4, height=3, pointsize=5, file=outs[i])
-    layout(t(1:2), widths=c(3,2))  # second panel is for the massive legend
+    devs[[i]](width=3.25, height=2, pointsize=5, file=outs[i])
+    par(mar=c(8,4,3,1))
+    barplot.oddsrats <- sort(odds.mat[which(xaxis==100),], decreasing=TRUE)
+    print(barplot.oddsrats)
+    barplot(barplot.oddsrats,
+        cex.names=0.8, las=3, border=F, col=colors[names(barplot.oddsrats)],
+        ylim=c(0,1.4), ylab='Odds ratio (OL odds / neuron odds)')
+    abline(h=1, lty='dashed')
+    dev.off()
+}
+
+
+# This is the supplementary figure with a range of N (10..500) for the top N
+# cancer genes.
+devs=list(pdf, svglite)
+outs=c(out.supp.pdf, out.supp.svg)
+for (i in 1:2) {
+    devs[[i]](width=6, height=3, pointsize=5, file=outs[i])
+    layout(t(1:2), widths=c(3,3))  # second panel is for the massive legend
 
     xlim <- c(0,500)
     ylim <- c(0.8,max(odds.mat, na.rm=T))
@@ -78,7 +104,34 @@ for (i in 1:2) {
     plot(x=0, y=0, pch=NA, bty='n', xaxt='n', yaxt='n', xlab='', ylab='')
     legend(x="center", legend=c(emphasize, types[!(types %in% emphasize)]),
         col=c(colors[emphasize], rep('grey', length(setdiff(types, emphasize)))),
-        lwd=2, bty="n")
+        lwd=2, bty="n", ncol=2)
+
+    dev.off()
+}
+
+# This is the supplementary figure with a full range of N for the top N
+# cancer genes.
+devs=list(pdf, svglite)
+outs=c(out.fullsupp.pdf, out.fullsupp.svg)
+for (i in 1:2) {
+    devs[[i]](width=6, height=3, pointsize=5, file=outs[i])
+    layout(t(1:2), widths=c(3,3))  # second panel is for the massive legend
+
+    xlim <- c(0,1e4)
+    ylim <- c(0.5,max(odds.mat, na.rm=T))
+    plot(x=0,y=0, xlim=xlim, ylim=ylim, bty='n', pch=NA,
+        xlab="Number of top genes",ylab="Odds ratio")
+    for (i in c(1:ncol(odds.mat), which(types %in% emphasize))) {
+        col <- colors[types[i]]
+        lwd <- ifelse(types[i] %in% emphasize, 2.5, 1/2)
+        lines(x=xaxis, y=odds.mat[,i], col=col, lwd=lwd)
+    }
+
+    # legend in its own panel
+    plot(x=0, y=0, pch=NA, bty='n', xaxt='n', yaxt='n', xlab='', ylab='')
+    legend(x="center", legend=c(emphasize, types[!(types %in% emphasize)]),
+        col=c(colors[emphasize], rep('grey', length(setdiff(types, emphasize)))),
+        lwd=2, bty="n", ncol=2)
 
     dev.off()
 }

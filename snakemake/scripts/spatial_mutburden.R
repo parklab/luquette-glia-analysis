7469b9009be53bf245a87222173e2b5be900502b
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-01-03 19:21:50 -0500

    Generalizations and allow for user-specified rolling window size

--- snakemake/scripts/spatial_mutburden.R
+++ snakemake/scripts/spatial_mutburden.R
@@ -10,8 +10,9 @@ if ('snakemake' %in% ls()) {
 
     commandArgs <- function(...) unlist(c(
         snakemake@input['muts'],
-        snakemake@input['binbed'],
         snakemake@input['meta'],
+        snakemake@input['binbed'],
+        snakemake@params['rolling_window_size'],
         snakemake@output['profile'],
         snakemake@output['stats'],
         snakemake@output['pdf'],
@@ -22,20 +23,21 @@ if ('snakemake' %in% ls()) {
 }
 
 args <- commandArgs(trailingOnly=TRUE)
-if (length(args) != 7) {
+if (length(args) != 8) {
     #cat('typically power is lower for finding regions with significant depletion.\n')
     #cat('excluding these regions from consideration a priori allows fewer hypothesis tests.\n')
-    stop('usage: spatial_mutburden.R muts_FILTERED.csv metadata.csv tiles.bed out.profile.csv out.stats.csv out.modelcomparison.pdf out.modelcomparison.svg')
+    stop('usage: spatial_mutburden.R muts_FILTERED.csv metadata.csv tiles.bed rolling_window_size out.profile.csv out.stats.csv out.modelcomparison.pdf out.modelcomparison.svg')
 }
 
 
 in.muts <- args[1]
 in.meta.csv <- args[2]
 in.bin.bed <- args[3]
-out.profile.csv <- args[4]
-out.stats.csv <- args[5]
-out.pdf <- args[6]
-out.svg <- args[7]
+bins.per.window <- as.integer(args[4])
+out.profile.csv <- args[5]
+out.stats.csv <- args[6]
+out.pdf <- args[7]
+out.svg <- args[8]
 
 for (f in c(out.profile.csv, out.stats.csv)) {
     if (file.exists(f))
@@ -51,7 +53,6 @@ if (!("Arial" %in% fonts()))
     stop("Arial font not detected; did you load extrafonts and run font_import() with the appropriate path?")
 
 padj.method <- 'BH'
-bins.per.window <- 10
 
 muts <- fread(in.muts)
 gmuts <- mutenrich::gr(muts)
@@ -78,7 +79,7 @@ bins$muts <- countOverlaps(bins, gmuts)
 # when the length of `filter` is even, the extra window is on the
 # right.
 # Result: sum of mutations over a sliding window of size 10 bins.
-bins$muts.smoothed <- stats::filter(bins$muts, filter=rep(1, bins.per.window), sides=2)
+bins$muts.smoothed <- as.numeric(stats::filter(bins$muts, filter=rep(1, bins.per.window), sides=2))
 bins$muts.smoothed.peryear <- bins$muts.smoothed / agesum
 
 
@@ -154,6 +155,7 @@ writeLines(text=c(paste0('AgeSum,', agesum),
   paste0('N bins(alignable),', length(bins)),
   paste0('Bin width(bp),', width(bins)[1]),
   paste0('Bins per sliding window,', bins.per.window),
+  paste0('N mutations,', nrow(muts)),
   paste0('Mutation type included,', unique(muts$muttype)),
   paste0('P-value adjustment method,', padj.method),
   paste0('Poisson GLM mean,', exp(coef(pois.model))),
@@ -172,7 +174,7 @@ outs=c(out.pdf, out.svg)
 for (i in 1:2) {
     devs[[i]](width=4.0, height=1.5, pointsize=5, file=outs[i])
     par(mar=c(4,4,1,0))
-    layout(t(1:3), widths=c(2,1,2))
+    layout(matrix(c(1:3,4,4,4), nrow=2, byrow=TRUE), widths=c(2,1,2))
     xmax <- max(bins$muts.smoothed, na.rm=TRUE)
     plot(as.numeric(names(table(bins$muts.smoothed))),
          as.vector(table(bins$muts.smoothed)/length(bins)),
@@ -194,24 +196,18 @@ for (i in 1:2) {
     # TODO: generalize these power calculations so they're done for every analysis
     # XXX: add enrich-only power testing
     if (FALSE) {
-    # This is the ALS total mutation average (snv+indel)
-    mu <- 11.1741396880185
-    # Actual number of mutations in the somatic catalog (SNV+indel)
-    # not used in any calculation, but might be nice to replace the
-    # "mut multiplier" by real counts.
-    n.muts <- 29109
-
-    layout(t(1:2))
+    mu <- exp(coef(pois.model))
     mults=c(1,2,5,10,20,50,100)
 
     for (i in 1:length(mults)) {
         mult = mults[i]
-        curve(ppois(q=qpois(p=0.05/25000, lambda=mu*mult), lambda=x*mu*mult), from=0.1,to=1,log='x', main='Power vs. effect size for depletion\n1MB smoothed at 100kb, Bonferroni', ylab='Power', xlab='Effect size', add=i>1, col=i, lwd=2, ylim=c(0,1))
+        curve(ppois(q=qpois(p=0.025/length(bins), lambda=mu*mult), lambda=x*mu*mult),
+            from=0.1,to=1,log='x', main='Power vs. effect size for depletion\n1MB smoothed at 100kb, Bonferroni', ylab='Power', xlab='Effect size', add=i>1, col=i, lwd=2, ylim=c(0,1))
     }
 
     for (i in 1:length(mults)) {
         mult = mults[i]
-        curve(1-ppois(q=qpois(p=1-0.05/25000, lambda=mu*mult, lower.tail=T), lambda=x*mu*mult), from=1,to=10,log='x', main='Power vs. effect size for enrichment\n1MB smoothed at 100kb, Bonferroni', ylab='Power', xlab='Effect size', add=i>1, col=i, lwd=2, ylim=c(0,1))
+        curve(1-ppois(q=qpois(p=1-0.025/length(bins), lambda=mu*mult, lower.tail=T), lambda=x*mu*mult), from=1,to=10,log='x', main='Power vs. effect size for enrichment\n1MB smoothed at 100kb, Bonferroni', ylab='Power', xlab='Effect size', add=i>1, col=i, lwd=2, ylim=c(0,1))
     }
     legend('bottomright', legend=mults, col=1:length(mults),lwd=2, title='Mutation multiplier')
     dev.off()

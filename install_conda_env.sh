f33a7f8d311779397f7d6d8cd3dd610372e55da5
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-05-29 18:19:31 -0400

    Initial commit

---
+++ install_conda_env.sh
@@ -0,0 +1,4 @@
+conda create -n glia_paper -c conda-forge -c bioconda -c jluquette -c dranew -c soil bcftools bedtools bioconductor-annotationdbi bioconductor-annotationfilter bioconductor-biovizbase bioconductor-bsgenome bioconductor-bsgenome.hsapiens.1000genomes.hs37d5 bioconductor-bsgenome.hsapiens.ucsc.hg19 bioconductor-bsgenome.hsapiens.ucsc.hg38 bioconductor-genomeinfodb bioconductor-genomeinfodbdata bioconductor-genomicalignments bioconductor-genomicfeatures bioconductor-genomicranges bioconductor-gviz bioconductor-txdb.hsapiens.ucsc.hg19.knowngene bioconductor-variantannotation bx-python mscorefonts pandas r-argparse r-curl r-devtools r-extrafont r-extrafontdb r-harmonicmeanp r-kernsmooth r-lme4 r-lmertest r-nnls r-openxlsx r-pheatmap r-rcolorbrewer r-readxl r-rttf2pt1 r-svglite s3transfer scan2 ucsc-bedgraphtobigwig ucsc-bigwigaverageoverbed
+
+
+echo "INSTALL MANUALLY: r-mutenrich, r-scan2 (if updated)"

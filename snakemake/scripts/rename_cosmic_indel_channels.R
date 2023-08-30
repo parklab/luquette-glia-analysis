35267833074ecb574bb28565e3320f06496e8eec
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-08-30 18:16:06 -0400

    Handle both SigProfilerMatrixGenerator and COSMIC channel names

--- snakemake/scripts/rename_cosmic_indel_channels.R
+++ snakemake/scripts/rename_cosmic_indel_channels.R
@@ -9,7 +9,8 @@ if ('snakemake' %in% ls()) {
     sink(con, type='message')
 
     commandArgs <- function(...) unlist(c(
-        snakemake@input[1], snakemake@output[1]
+        snakemake@input['cosmic'],
+        snakemake@output['csv']
     ))
     cat('Got command line arguments from snakemake:\n')
     print(commandArgs())
@@ -17,7 +18,7 @@ if ('snakemake' %in% ls()) {
 
 args <- commandArgs(trailingOnly=TRUE)
 if (length(args) != 2) {
-    stop("usage: make_cosmic_indel_table.R in_cosmic.csv out.csv")
+    stop("usage: rename_cosmic_indel_channels.R in_cosmic.csv out.csv")
 }
 
 
@@ -27,7 +28,7 @@ outcsv <- args[2]
 if (file.exists(outcsv))
     stop(paste('output file', outcsv, 'already exists, please delete it first'))
 
-library(scan2)
+suppressMessages(library(scan2))
 
 # N.B.
 # Unlike COSMIC SBS, the indel database has no " -    " values
@@ -37,20 +38,30 @@ head(cosmic)
 
 id83 <- levels(id83(c()))
 
-# In the COSMIC database, ID83 channel names are formatted differently
-# from SigProfilerMatrixGenerator (spmgr). E.g.:
-# COSMIC
-# [1] "DEL_C_1_0"  "DEL_C_1_1"  "DEL_C_1_2"  "DEL_C_1_3"  "DEL_C_1_4" 
-# [6] "DEL_C_1_5+" ...
-# SPMGR
-# [1] "1:Del:C:0" "1:Del:C:1" "1:Del:C:2" "1:Del:C:3" "1:Del:C:4" "1:Del:C:5"
-# This function translates COSMIC names -> SPMGR names
-id83.cosmic.to.spmgr <- function(x) {
-    sapply(strsplit(sub('MH', 'M', gsub('\\+', '', gsub('_', ':', sub('repeats', 'R', sub('DEL', 'Del', sub('INS', 'Ins', x)))))), ':'), function(elts) paste(elts[3], elts[1], elts[2], elts[4], sep=':'))
+if (colnames(cosmic)[1] == 'Mutation Type') {
+    # The COSMIC signatures downloaded from the website have the following ID83 channel
+    # names.  The column is also called "Mutation Type", with a space.
+    # In the COSMIC database, ID83 channel names are formatted differently
+    # from SigProfilerMatrixGenerator (spmgr). E.g.:
+    # COSMIC
+    # [1] "DEL_C_1_0"  "DEL_C_1_1"  "DEL_C_1_2"  "DEL_C_1_3"  "DEL_C_1_4" 
+    # [6] "DEL_C_1_5+" ...
+    # SPMGR
+    # [1] "1:Del:C:0" "1:Del:C:1" "1:Del:C:2" "1:Del:C:3" "1:Del:C:4" "1:Del:C:5"
+    # This function translates COSMIC names -> SPMGR names
+    id83.cosmic.to.spmgr <- function(x) {
+        sapply(strsplit(sub('MH', 'M', gsub('\\+', '', gsub('_', ':', sub('repeats', 'R', sub('DEL', 'Del', sub('INS', 'Ins', x)))))), ':'), function(elts) paste(elts[3], elts[1], elts[2], elts[4], sep=':'))
+    }
+    
+    cosmic[[1]] <- id83.cosmic.to.spmgr(cosmic[[1]])
+    colnames(cosmic)[1] <- 'MutType'
+} else {
+    # The active COSMIC signatures output by SigProfilerExtractor use the same channel
+    # name scheme as SCAN2 (because SCAN2 uses SigProfilerMatrixGenerator internally).
+    # Just rename the column to match expectation.
+    colnames(cosmic)[1] <- 'MutType'
 }
 
-cosmic[[1]] <- id83.cosmic.to.spmgr(cosmic[[1]])
-colnames(cosmic)[1] <- 'MutType'
 setkey(cosmic, MutType)
 cosmic <- cosmic[id83]
 head(cosmic)

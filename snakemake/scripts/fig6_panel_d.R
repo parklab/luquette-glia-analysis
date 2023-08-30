f5acf07d8ca409cb367724d00fb3b1a78343f142
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-08-30 18:02:24 -0400

    Allow more flexible group definition

--- snakemake/scripts/fig6_panel_d.R
+++ snakemake/scripts/fig6_panel_d.R
@@ -9,61 +9,47 @@ if ('snakemake' %in% ls()) {
     sink(con, type='message')
 
     commandArgs <- function(...) unlist(c(
+        snakemake@input['csv'],
         snakemake@output['pdf'],
         snakemake@output['svg'],
-        snakemake@output['csv'],
         snakemake@output['supppdf'],
         snakemake@output['suppsvg'],
         snakemake@output['fullsupppdf'],
-        snakemake@output['fullsuppsvg'],
-        snakemake@input['cancer_ors']
+        snakemake@output['fullsuppsvg']
     ))
     cat('Got command line arguments from snakemake:\n')
     print(commandArgs())
 }
 
 args <- commandArgs(trailingOnly=TRUE)
-if (length(args) < 8) {
-    stop('usage: fig4_panel_d.R out.pdf out.svg out.csv out.supp.pdf out.supp.svg out.fullsupp.pdf out.fullsupp.svg cancer_odds_ratios1.rda [ cancer_odd_ratios2.rda ... cancer_odds_ratiosN.rda ]')
+if (length(args) != 7) {
+    stop('usage: fig6_panel_d.R odds_ratio_matrix.csv out.pdf out.svg out.supp.pdf out.supp.svg out.fullsupp.pdf out.fullsupp.svg')
 }
 
 
-out.pdf <- args[1]
-out.svg <- args[2]
-out.csv <- args[3]
+in.csv <- args[1]
+out.pdf <- args[2]
+out.svg <- args[3]
 out.supp.pdf <- args[4]
 out.supp.svg <- args[5]
 out.fullsupp.pdf <- args[6]
 out.fullsupp.svg <- args[7]
-cancer.fs <- args[-(1:7)]
 
-for (f in c(out.pdf, out.svg, out.csv, out.supp.pdf, out.supp.svg, out.fullsupp.pdf, out.fullsupp.svg)) {
+for (f in c(out.pdf, out.svg, out.supp.pdf, out.supp.svg, out.fullsupp.pdf, out.fullsupp.svg)) {
     if (file.exists(f))
         stop(paste('output file', f, 'already exists, please delete it first'))
 }
 
 suppressMessages(library(data.table))
 suppressMessages(library(mutenrich))
-suppressMessages(library(extrafont))
 suppressMessages(library(svglite))
-if (!("Arial" %in% fonts()))
-    stop("Arial font not detected; did you load extrafonts and run font_import() with the appropriate path?")
 
-data <- lapply(cancer.fs, function(f) {
-    load(f)  # among other things, loads 'tops', 'types' and 'fts'
-    list(tops=tops, types=types, odds=fts[1,])
-})
-odds.mat <- sapply(1:length(data), function(i) data[[i]]$odds)
-colnames(odds.mat) <- sapply(data, function(d) d$types)
-types <- colnames(odds.mat)
-xaxis <- data[[1]]$tops
-
-x <- data.table(TopNGenes=xaxis, odds.mat)[order(TopNGenes)]
-fwrite(x, file=out.csv)
-
+odds.mat <- fread(in.csv)
+types <- colnames(odds.mat)[-1]
+xaxis <- odds.mat$TopNGenes
 
 emphasize <- c('CNS-GBM','CNS-Medullo','CNS-Oligo', 'CNS-PiloAstro')
-colors <- setNames(rep('grey',37), colnames(odds.mat))
+colors <- setNames(rep('grey',37), types)
 colors[emphasize] <- c('orange','black','red','purple')
 
 
@@ -72,7 +58,7 @@ outs=c(out.pdf, out.svg)
 for (i in 1:2) {
     devs[[i]](width=2, height=3.25, pointsize=5, file=outs[i])
     par(mar=c(4,8,0.1,1))
-    barplot.oddsrats <- sort(odds.mat[which(xaxis==100),], decreasing=FALSE)
+    barplot.oddsrats <- sort(setNames(as.numeric(odds.mat[TopNGenes==100,-1]), types), decreasing=FALSE)
     print(barplot.oddsrats)
     barplot(barplot.oddsrats,
         cex.names=0.8, border=F, col=colors[names(barplot.oddsrats)],
@@ -91,13 +77,13 @@ for (i in 1:2) {
     layout(t(1:2), widths=c(3,3))  # second panel is for the massive legend
 
     xlim <- c(0,500)
-    ylim <- c(0.8,max(odds.mat, na.rm=T))
+    ylim <- c(0.8,max(as.matrix(odds.mat[,-1]), na.rm=T))
     plot(x=0,y=0, xlim=xlim, ylim=ylim, bty='n', pch=NA,
         xlab="Number of top genes",ylab="Odds ratio")
-    for (i in c(1:ncol(odds.mat), which(types %in% emphasize))) {
+    for (i in c(2:ncol(odds.mat), which(types %in% emphasize))) {
         col <- colors[types[i]]
         lwd <- ifelse(types[i] %in% emphasize, 2.5, 1/2)
-        lines(x=xaxis, y=odds.mat[,i], col=col, lwd=lwd)
+        lines(x=xaxis, y=odds.mat[[i]], col=col, lwd=lwd)
     }
 
     # legend in its own panel
@@ -118,13 +104,13 @@ for (i in 1:2) {
     layout(t(1:2), widths=c(3,3))  # second panel is for the massive legend
 
     xlim <- c(0,1e4)
-    ylim <- c(0.5,max(odds.mat, na.rm=T))
+    ylim <- c(0.5,max(as.matrix(odds.mat[,-1]), na.rm=T))
     plot(x=0,y=0, xlim=xlim, ylim=ylim, bty='n', pch=NA,
         xlab="Number of top genes",ylab="Odds ratio")
-    for (i in c(1:ncol(odds.mat), which(types %in% emphasize))) {
+    for (i in c(2:ncol(odds.mat), which(types %in% emphasize))) {
         col <- colors[types[i]]
         lwd <- ifelse(types[i] %in% emphasize, 2.5, 1/2)
-        lines(x=xaxis, y=odds.mat[,i], col=col, lwd=lwd)
+        lines(x=xaxis, y=odds.mat[[i]], col=col, lwd=lwd)
     }
 
     # legend in its own panel

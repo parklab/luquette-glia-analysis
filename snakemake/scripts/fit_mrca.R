9dc6f3c3787ef7a7f2794d1644a049f9faf694cc
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-12-24 09:04:27 -0500

    New method for estimating time to MRCA: only use the shared mutations, which contain fewer FPs than private muts. Estimate an interval by considering how uncertainty in the aging trend model would affect the MRCA timing. Add the "indeterminate" class for hSNPs that fail the alt/dp reqs. Although private timing is no longer used, actually use the corrected private count rather tha inadvertently ignoring it.

--- snakemake/scripts/fit_mrca.R
+++ snakemake/scripts/fit_mrca.R
@@ -65,6 +65,10 @@ combine.dfs <- function(s1, s2) {
     d1$private <- !d1$shared & (d1$pass1 | d1$pass2) &
         (d1$alt1 == 0 | d1$alt2 == 0) &   # added may 25 2022
         (d1$dp1 > 5 & d1$dp2 > 5)         # added may 25 2022
+    # anything not shared or private, but must be passed somewhere
+    d1$indeterminate <- (d1$pass1 | d1$pass2) & !d1$shared & !d1$private  
+    # labels are just convenient for tabling and grouping
+    d1$classification <- ifelse(d1$shared, 'shared', ifelse(d1$private, 'private', ifelse(d1$indeterminate, 'indeterminate', 'none')))
     d1
 }
 
@@ -88,7 +92,8 @@ gc()
 
 x <- combine.dfs(r1@gatk[somatic.candidate == TRUE & muttype == 'snv'],
                  r2@gatk[somatic.candidate == TRUE & muttype == 'snv'])
-muttab <- x[shared == TRUE | private == TRUE]
+#muttab <- x[shared == TRUE | private == TRUE]
+muttab <- x[classification != 'none']
 
 fwrite(muttab, file=out.muts.csv)
 
@@ -109,25 +114,41 @@ get.sbs1 <- function(tab) {
 # cell corresponding to `object`!!
 # override n.shared and n.private to do SBS1 modeling
 # this.cosmic is the global cosmic plus either the PTA artifact signature or MDA artifact signature (sigB)
-f <- function(object, amptype, this.cosmic, muttab, hsnptab, model.intercept, model.rate, remove.sigb=FALSE,
+f <- function(object, amptype, this.cosmic, muttab, hsnptab, model.intercept, model.intercept.lb, model.intercept.ub, model.rate, model.rate.lb, model.rate.ub, remove.sigb=FALSE,
     n.shared.fn=nrow, n.private.fn=nrow)
 {
     sample <- object@single.cell
-    burden <- object@mutburden$snv[2,]$burden
-print('a')
+    burden <- mutburden(object)['snv']
     n.shared <- n.shared.fn(muttab[shared == TRUE])
-print('b')
     n.private <- n.private.fn(muttab[private == TRUE])
 
     # Quantity is: number of shareds misclassified as a function of total shared count.
     # This doesn't matter much for PTA, but does for MDA.
     # When doing SBS1, no need to estimate SBS1 level and/or restrict hSNPs to SBS1-like muts
     print(table(private=hsnptab$private, shared=hsnptab$shared))
-    shared.as.private.rate <- sum(hsnptab$private == TRUE, na.rm=TRUE) / sum(hsnptab$shared == TRUE, na.rm=TRUE)
+
+    # this doesn't make sense, but also we need to account for indeterminate
+    #shared.as.private.rate <- sum(hsnptab$private == TRUE, na.rm=TRUE) / sum(hsnptab$shared == TRUE, na.rm=TRUE)
+    # all germline hSNPs are shared mutations. anything not classified as shared is considered
+    # a misclassification (whether it is indeterminate or private).
+    shared.misclassification.rate <- hsnptab[, mean(classification != 'shared')]
 
     # Shift the estimated number of shared mutations accidentally misclassified as private
     # from the private count to the shared count.
-    n.shared.adj <- n.shared * (1 + shared.as.private.rate)
+    # 
+    # T: unobserved.  True number of shared mutations between cells (unobserved)
+    # S: observed.    Number of shared mutations detected by the heuristic (observed)
+    # f: inferred.    Fraction of truly shared mutations T misclassified by heuristic as private or indeterminate
+    # fT:             Number of misclassified shared SNVs as private or indet.
+    # 
+    # Total number of shared mutations is the sum of the ones we detect as shared and
+    # those misclassified as private.
+    #
+    #   T = S + fT
+    #   T - fT = S
+    #   T(1 - f) = S
+    #   T = S/(1 - f)
+    n.shared.adj <- n.shared / (1 - shared.misclassification.rate)
     n.private.adj <- n.private - (n.shared.adj - n.shared)
 
     # For MDA samples, private mutations will have signature B artifacts.
@@ -143,19 +164,61 @@ print('b')
 
     # Extrapolate rates to whole genome burden. This matches the rates used
     # in the aging models.
-    n.shared.final <- n.shared * mut.to.burden.rate
-    n.private.final <- n.private * mut.to.burden.rate
+    n.shared.final <- n.shared.adj * mut.to.burden.rate
+    n.private.final <- n.private.adj2 * mut.to.burden.rate
 
+    # New method: use the variability in the aging model's intercept/yearly rate to
+    # create an interval on the number of shared years. This method is probably better
+    # than the original count-forward, count-backward method because shared mutations
+    # are (1) far more likely to not be artifactual and (2) it is very easy to model
+    # sensitivity since all hSNPs are also shared and can be used to accurately
+    # determine what fraction of shared sites should be recovered.
+    #
+    # All estimates are of the form:
+    #
+    #    years  =  (# of sensitivity adjusted shared mutations - INTERCEPT) / RATE
+    #
+    # where INTERCEPT and RATE each take on the point estimate from the linear model
+    # and the 95% CI upper and lower bounds.
+
+    # Get all possible combinations of upper and lower bounds. The min and max values
+    # don't correspond to min and max values for the final range because the values
+    # (years) can be negative. year=0 means birth, so there are still 9 months
+    # preceding that.
+    grid <- expand.grid(c(model.intercept.lb, model.intercept.ub),
+                c(model.rate.lb, model.rate.ub))
+    ests <- apply(grid, 1, function(row) (n.shared.final - row[1]) / row[2])
+    cat("all estimates using 95% CI bounds:\n")
+    print(ests)
+
+    # As explained above, these are not actually the min/max values because n.shared.final-intercept can be <0
+    # Maximum (=upper bounds) INTERCEPT and RATE => minimum value for years (=lower bound)
+    #years.shared.lb <- (n.shared.final - model.intercept.ub) / model.rate.ub
+    # Minimum (=lower bounds) INTERCEPT and RATE => maximum value for years (=upper bound)
+    #ears.shared.ub <- (n.shared.final - model.intercept.lb) / model.rate.lb
+    years.shared.lb <- min(ests)
+    years.shared.ub <- max(ests)
     years.shared <- (n.shared.final - model.intercept) / model.rate
+
+    # no longer relevant, but kept around for posterity
     years.private <- (n.private.final) / model.rate
     years.total <- (n.shared.final + n.private.final - model.intercept) / model.rate
 
-    data.frame(sample=sample, amptype=amptype, sigb=sigb, shared.as.private.rate=shared.as.private.rate,
+    data.frame(sample=sample, amptype=amptype, sigb=sigb, shared.misclassification.rate=shared.misclassification.rate,
         burden=burden, burden.adj=burden.adj, mut.to.burden.rate=mut.to.burden.rate,
         n.shared=n.shared, n.shared.adj=n.shared.adj, n.shared.final=n.shared.final,
         n.private=n.private, n.private.adj=n.private.adj, n.private.adj2=n.private.adj2, n.private.final=n.private.final,
-        model.intercept=model.intercept, model.rate=model.rate,
-        years.shared=years.shared, years.private=years.private, years.total=years.total)
+        model.intercept=model.intercept,
+        model.intercept.lb=model.intercept.lb,
+        model.intercept.ub=model.intercept.ub,
+        model.rate=model.rate,
+        model.rate.lb=model.rate.lb,
+        model.rate.ub=model.rate.ub,
+        years.shared=years.shared,
+        years.shared.lb=years.shared.lb,
+        years.shared.ub=years.shared.ub,
+        years.private=years.private,   # these are no longer relevant
+        years.total=years.total)       #
 }
 
 # Date MRCA based on total mutation burden
@@ -175,7 +238,11 @@ total.mut <- data.frame(model='Total burden', all.shared=sum(muttab$shared),
             muttab=muttab[pass1 == TRUE],
             hsnptab=hsnptab[pass1 == TRUE],
             model.intercept=aging.burden.model[Variable=='(Intercept)', Estimate],
+            model.intercept.lb=aging.burden.model[Variable=='(Intercept)', `95% CI lower`],
+            model.intercept.ub=aging.burden.model[Variable=='(Intercept)', `95% CI upper`],
             model.rate=aging.burden.model[Variable=='age', Estimate],
+            model.rate.lb=aging.burden.model[Variable=='age', `95% CI lower`],
+            model.rate.ub=aging.burden.model[Variable=='age', `95% CI upper`],
             remove.sigb=amptype1 == 'MDA'),
         f(object=r2,
             amptype=amptype2,
@@ -183,7 +250,11 @@ total.mut <- data.frame(model='Total burden', all.shared=sum(muttab$shared),
             muttab=muttab[pass2 == TRUE],
             hsnptab=hsnptab[pass2 == TRUE],
             model.intercept=aging.burden.model[Variable=='(Intercept)', Estimate],
+            model.intercept.lb=aging.burden.model[Variable=='(Intercept)', `95% CI lower`],
+            model.intercept.ub=aging.burden.model[Variable=='(Intercept)', `95% CI upper`],
             model.rate=aging.burden.model[Variable=='age', Estimate],
+            model.rate.lb=aging.burden.model[Variable=='age', `95% CI lower`],
+            model.rate.ub=aging.burden.model[Variable=='age', `95% CI upper`],
             remove.sigb=amptype2 == 'MDA')))
 
 # Date MRCA based on SBS1
@@ -194,7 +265,11 @@ sbs1.mut <- data.frame(model='SBS1 burden', all.shared=NA,
             muttab=muttab[pass1 == TRUE],
             hsnptab=hsnptab[pass1 == TRUE],
             model.intercept=aging.burden.model.sigs[Variable == '(Intercept)', Estimate],
+            model.intercept.lb=aging.burden.model.sigs[Variable=='(Intercept)', `95% CI lower`],
+            model.intercept.ub=aging.burden.model.sigs[Variable=='(Intercept)', `95% CI upper`],
             model.rate=aging.burden.model.sigs[Variable == 'age', Estimate],
+            model.rate.lb=aging.burden.model.sigs[Variable=='age', `95% CI lower`],
+            model.rate.ub=aging.burden.model.sigs[Variable=='age', `95% CI upper`],
             n.shared.fn=get.sbs1, n.private.fn=get.sbs1),
         f(object=r2,
             amptype=amptype2,
@@ -202,7 +277,11 @@ sbs1.mut <- data.frame(model='SBS1 burden', all.shared=NA,
             muttab=muttab[pass2 == TRUE],
             hsnptab=hsnptab[pass2 == TRUE],
             model.intercept=aging.burden.model.sigs[Variable == '(Intercept)', Estimate],
+            model.intercept.lb=aging.burden.model.sigs[Variable=='(Intercept)', `95% CI lower`],
+            model.intercept.ub=aging.burden.model.sigs[Variable=='(Intercept)', `95% CI upper`],
             model.rate=aging.burden.model.sigs[Variable == 'age', Estimate],
+            model.rate.lb=aging.burden.model.sigs[Variable=='age', `95% CI lower`],
+            model.rate.ub=aging.burden.model.sigs[Variable=='age', `95% CI upper`],
             n.shared.fn=get.sbs1, n.private.fn=get.sbs1)))
 
 results <- rbind(total.mut, sbs1.mut)
